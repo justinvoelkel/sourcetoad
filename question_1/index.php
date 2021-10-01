5211df5e157038eec4452ea91a2e70660237143b
Justin Voelkel <justinv@integrityenergy.com>
2021-10-01 01:32:23 -0400

    quick clean up and comment on question 1

--- question_1/index.php
+++ question_1/index.php
@@ -7,12 +7,15 @@
    Given the following example data structure. Write a single function to print out its nested key value pairs at any level for easy display to the user.
 */
 
-  function print_nested($arr, $indent = "") {
+  function print_nested($arr, $indent = "")
+  {
       foreach($arr as $key => $val) {
           echo $indent;
+          // skip the numeric index of the top level array
           if(!is_int($key)){
               echo "$key: ";
           }
+          // if the value is array call self and further indent
           if(is_array($val)){
               echo "\r\n";
               print_nested($val, $indent . "\t");
